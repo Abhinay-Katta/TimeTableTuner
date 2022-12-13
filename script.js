865eeaf9833ad540a7a395829e3bd4917a5d86fb
Abhinay Katta <abhinaykatta01108@gmail.com>
2022-12-14 01:02:07 +0530

    load>click

--- script.js
+++ script.js
@@ -20,43 +20,62 @@ function printDay() {
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',]
     document.getElementById("pd").innerHTML = days[day];
 }
+function linke() {
+    const sub = printClass();
+    if (sub == 'Cyber Security') {
+
+        window.location.href = 'https://bytexl-in.zoom.us/meeting/register/tZcod-GtqjgpE9GAb-ABO-3K7oTlyt2DVudD';
+    }
+
+    else if (sub == 'Recess' || sub == 'Lunch Break') {
+        document.getElementById("link").innerHTML = "Break";
+    }
+    else {
+        Document.getElementById("link").innerHTML = "Offline Class";
+    }
+}
 function printClass() {
     const day = findDay();
     const hr = d.getHours();
     const min = d.getMinutes();
+    var sub = '';
     var time = hr * 60 + min;
-    /*test:
-    if (time > 490 && time < 500) {
-        document.getElementById("day").innerHTML = 'this is ass';
-    }*/
     if (time > 585 && time < 645) {
-        document.getElementById("day").innerHTML = tt[day][0];
+        sub += tt[day][0];
     }
     else if (time > 645 && time < 705) {
-        document.getElementById("day").innerHTML = tt[day][1];
+        sub += tt[day][1];
     }
     else if (time > 705 && time < 765) {
-        document.getElementById("day").innerHTML = 'Lunch Break';
+        sub += 'Lunch Break';
     }
     else if (time > 765 && time < 815) {
-        document.getElementById("day").innerHTML = tt[day][2];
+        sub += tt[day][2];
     }
     else if (time > 815 && time < 865) {
-        document.getElementById("day").innerHTML = tt[day][3];
+        sub += tt[day][3];
     }
     else if (time > 865 && time < 885) {
-        document.getElementById("day").innerHTML = 'Recess';
+        sub += 'Recess';
     }
     else if (time > 885 && time < 945) {
-        document.getElementById("day").innerHTML = tt[day][4];
+        sub += tt[day][4];
     }
     else if (time > 945 && time < 1005) {
-        document.getElementById("day").innerHTML = tt[day][5];
+        sub += tt[day][5];
     }
     else if (time < 585) {
-        document.getElementById("day").innerHTML = "Classes have not started yet";
+        sub += "Classes have not started yet";
     }
     else if (time > 1005) {
-        document.getElementById("day").innerHTML = "Classes have ended";
+        sub += "Classes have ended";
     }
+    document.getElementById("day").innerHTML = sub;
+    return sub, day;
+}
+function nextClass() {
+    const current_sub = printClass()[0];
+    const current_day = printClass()[1];
+    document.getElementById("next").innerHTML += tt[current_day].values(current_sub);
+    // document.getElementById("next").innerHTML = tt[current_day[current_sub]];
 }
